68b43f68a2d4c3fbd494ba28588f0eaf1e52e737
ARUN A MENON <52127560+arunamenon@users.noreply.github.com>
2024-12-14 16:34:13 +0000

    updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   skip_forgery_protection
 end
+
+class MyController < ApplicationController
+  def zebra
+    redirect_to("https://www.wikipedia.org/wiki/HTTP_303")
+  end
+end
+
+class MyController < ApplicationController
+  def zebra
+    redirect_to("/wiki/HTTP_303") # Assuming we have a route that matches this
+  end
+end
